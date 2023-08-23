fadd88e18e6adb4f38265798593729d3b58f2cc1
JustCheka <cheka201@yandxe.ru>
2023-08-23 10:56:08 +0300

    Ex 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+
+string num1 = "";
+
+for (int i = num.Length - 1; i >= 0; i--){
+    num1 += num[i];
+}
+
+if (int.Parse(num1) == int.Parse(num)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
